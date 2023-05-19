8b544ba2b88a568684ce3d72702df1a56cbe77fc
Akbar <akbarputrasrg@gmail.com>
2023-05-19 20:53:22 +0700

    controller

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,41 @@ Route::get('/world', function () {
 
 Route::redirect('/hi', '/hello');
 
-Route::fallback(function() {
+Route::get('/products/{id}', function ($id) {
+    return 'Product ID: ' . $id;
+})->name('product.detail');
+
+Route::get('/products/{productId}/items/{itemId}', function ($productId, $itemId) {
+    return 'Product ID: ' . $productId . ', Items: ' . $itemId;
+})->name('product.item.detail');
+
+Route::get('/categories/{id}', function (string $categoryId) {
+    return "Category ID: $categoryId";
+})->where('id', '[0-9]+')->name('category.detail');
+
+Route::get('/users/{id?}', function (string $userId = '404') {
+    return "User ID: $userId";
+})->name('user.detail');
+
+Route::get('/conflict/hello', function () {
+    return "Conflict Hello World";
+});
+
+Route::get('/conflict/{name}', function (string $name) {
+    return "Conflict $name";
+});
+
+Route::get('/produk/{id}', function (string $id) {
+    $link = route('product.detail', ['id' => $id]);
+    return "Link $link";
+});
+
+Route::get('/product-redirect/{id}', function (string $id) {
+    return redirect()->route('product.detail', ['id' => $id]);
+});
+
+Route::get('/controller/hello/{name}', [App\Http\Controllers\HelloController::class, 'hello']);
+
+Route::fallback(function () {
     return "404 Not Found";
 });
--- tests/Feature/RoutingTest.php
+++ tests/Feature/RoutingTest.php
@@ -28,4 +28,60 @@ class RoutingTest extends TestCase
             ->assertSeeText('404 Not Found');
     }
 
+    public function testRouteParameter()
+    {
+        $this->get('/products/1')
+            ->assertStatus(200)
+            ->assertSeeText('Product ID: 1');
+
+        $this->get('/products/1/items/2')
+            ->assertStatus(200)
+            ->assertSeeText('Product ID: 1, Items: 2');
+    }
+
+    public function testRouteParameterRegex()
+    {
+        $this->get('/categories/1')
+            ->assertStatus(200)
+            ->assertSeeText('Category ID: 1');
+
+        $this->get('/categories/abc')
+            ->assertStatus(200)
+            ->assertSeeText('404 Not Found');
+    }
+
+    public function testRouteOptionalParameter()
+    {
+        $this->get('/users/')
+            ->assertStatus(200)
+            ->assertSeeText('User ID: 404');
+
+        $this->get('/users/1')
+            ->assertStatus(200)
+            ->assertSeeText('User ID: 1');
+    }
+
+    public function testRoutingConflict()
+    {
+        $this->get('/conflict/hello')
+            ->assertStatus(200)
+            ->assertSeeText('Conflict Hello World');
+
+        $this->get('/conflict/john')
+            ->assertStatus(200)
+            ->assertSeeText('Conflict john');
+    }
+
+    public function testNamedRoute()
+    {
+        $this->get('/produk/1')
+            ->assertStatus(200)
+            ->assertSeeText('/products/1');
+
+        $this->get('/product-redirect/1')
+            ->assertStatus(302)
+            ->assertRedirect('/products/1');
+    }
+
+
 }
